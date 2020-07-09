77b66729f03e8006157922cf8761f422b7133b4d
ledoyen <ledoyen.loic@gmail.com>
2020-07-09 23:00:53 +0200

    Add utility to list signatures from a JAR archive

--- core/src/test/java/com/github/fridujo/retrokompat/JarObjectTests.java
+++ core/src/test/java/com/github/fridujo/retrokompat/JarObjectTests.java
@@ -2,6 +2,8 @@ package com.github.fridujo.retrokompat;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Executable;
 import java.nio.file.Path;
 import java.util.Set;
 
@@ -12,15 +14,22 @@ import com.github.fridujo.retrokompat.tools.PathUtils;
 
 class JarObjectTests {
 
+    private final Path jarPath = JarMaker.compileAndPackage(PathUtils.forClassPath("jar_files/two_classes"));
+
     @Test
     void listClassNames_returns_qualified_names() {
-        Path jarPath = JarMaker.compileAndPackage(PathUtils.forClassPath("jar_files/two_classes"));
-
         Set<String> classNames = new JarObject(jarPath).listClassNames();
 
         assertThat(classNames)
             .containsExactlyInAnyOrder(
                 "test.SampleClass1",
                 "test.SampleClass2");
+    }
+
+    @Test
+    void extractSignatures_returns_public_signatures() {
+        Set<Signature> signatures = new JarObject(jarPath).extractSignatures();
+
+        assertThat(signatures.stream().map(s -> s.executable)).hasSize(2);
     }
 }
--- core/src/test/resources/jar_files/two_classes/test/SampleClass2.java
+++ core/src/test/resources/jar_files/two_classes/test/SampleClass2.java
@@ -2,4 +2,10 @@ package test;
 
 public class SampleClass2 {
 
+    protected SampleClass2() {
+    }
+
+    public String getSomething() {
+        return "something";
+    }
 }
